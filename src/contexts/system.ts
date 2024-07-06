c925bb891584d9783658cbabe12fcbb9df88eb2d
Jose Junior <joseluizjunior@yahoo.com>
2024-07-06 19:59:57 -0300

    update variavel de ambiente

--- src/contexts/system.ts
+++ src/contexts/system.ts
@@ -8,8 +8,8 @@ try {
   console.error('Error parsing manifest JSON:', error);
 }
 
-export const BASE_URL = extra.REACT_APP_BASE_URL || 'http://192.168.1.3:8080';
-export const CLIENT_ID = extra.REACT_APP_CLIENT_ID || 'myclientid';
-export const CLIENT_SECRET = extra.REACT_APP_CLIENT_SECRET || 'myclientsecret';
+export const BASE_URL = extra.REACT_APP_BASE_URL ;
+export const CLIENT_ID = extra.REACT_APP_CLIENT_ID ;
+export const CLIENT_SECRET = extra.REACT_APP_CLIENT_SECRET;
 export const TOKEN_KEY = 'com.esibape/token';
 
